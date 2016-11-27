15e627f98e719ea42cacd982f6e431f08fd0d7c1
Sam Goree <sgoree@oberlin.edu>
2016-11-26 22:39:25 -0500

    fixes to encoder

--- encoder.py
+++ encoder.py
@@ -6,20 +6,20 @@ class encoder():
     #strings map to strings or floats, aka attribute category to values. Createa  function that takes a dataset, aka orderedDicts, and a list of attribute names, and
     #converts each orderedDict entry to a 2D array. Result should be a 3D array, use numpy.
     uniqueValues = []
-    values = []
+    values = {}
     
     def decode(self, code, attributes):    #We can send this method the 1hotencoding and it should return the values associated with it.
-        vMap = []
+        vMap = {}
         for item in code:
             counter = 0
             for attribute in attributes:
-                for value in self.values(attribute):
-                    if(code[counter]==1):
-                            vMap.append(attribute)
-                            vMap[attribute].append(value)
+                for value in self.values[attribute]:
+                    if code[counter]==1:
+                            vMap[attribute] = value
                     counter = counter +1
+        return vMap
     
-    def skconvert(self, listDic, attributes): #Given a dictionary of attributes->values, and the list of attributes, and then output a 2D array, 
+    def encode(self, listDic, attributes): #Given a dictionary of attributes->values, and the list of attributes, and then output a 2D array, 
             # in addition to the desired outputs for encoding, we also want to build a mapping that allows us to decode later. 
             # It would be <index of encoding, <attribute, value>>
             
@@ -31,33 +31,29 @@ class encoder():
             #We need a list of all of the unique values across all attributes, as well as a mapping of attribute->list of possible values.
             for instance in listDic:
                 for attribute in attributes:
-                    if attribute not in self.values:     
-                        self.values.append(attribute)
+                    if attribute not in self.values.keys():     
+                        self.values[attribute] = []
                     if instance[attribute] not in self.values[attribute]:
                         self.values[attribute].append(instance[attribute])
                         self.uniqueValues.append(instance[attribute])
         
-            resultA = numpy.zeros(len(listDic),len(self.uniqueValues))
-            resultL = numpy.zeros(len(listDic),len(self.uniqueValues))
-        
-            #If, for each instance, we iterate through attribute in attributes, check the index of each attribute in attributes, and then check that index in the instances'
-            # list of values, then that might work? 
-            #I don't see how to do this, we need to have subsets of the uniqueValues, so that it's categorized by attribute.
-            for attribute in attributes:
-                dmap.append(attribute)
-                for value in self.values(attribute):
-                    dmap[attribute].append(value)
+            resultA = [[] for l in listDic]
+            resultL = [[] for l in listDic]
                     
-            for instance in listDic:
+            for i,instance in enumerate(listDic):
                 for attribute in attributes:
                     for v in self.values[attribute]: #We need a list of all the values for a given attribute.
                         if attribute == "label":
                             for label in self.values["label"]:
                                 if label == v:
-                                    resultL[instance].append(1)   
+                                    resultL[i].append(1)
+                                else:
+                                    resultL[i].append(0)
                         else:
                             if instance[attribute] == v:
-                                resultA[instance].append(1)
+                                resultA[i].append(1)
+                            else:
+                                resultA[i].append(0)
             #Now we should have a 2d array of instances mapping to 1hotencoding of attributes for that instance.    
             #return [instances, 1hotencoding of attributes] and [instances, 1hotencoding of label]
             return resultA, resultL
